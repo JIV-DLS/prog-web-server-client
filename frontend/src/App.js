5a6d4de53d4719a06d4938c6bb4c4d9a2b75d924
hamzaayoub98 <hamza.ayoub@etu.univ-cotedazur.fr>
2022-02-07 17:39:45 +0100

    Add Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MyMap from './componenents/myMap';
 
 function App() {
+
+  const mapIsReadyCallback = (map) => {
+    console.log(map);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyMap mapIsReadyCallback={mapIsReadyCallback}/>
   );
 }
 
