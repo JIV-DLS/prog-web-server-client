64458caeaa88e199f6664c7de51735631a5bf108
hamzaayoub98 <hamza.ayoub@etu.univ-cotedazur.fr>
2022-02-14 20:21:58 +0100

    Récupérer le centre de la map

--- frontend/src/components/myMap.jsx
+++ frontend/src/components/myMap.jsx
@@ -3,23 +3,39 @@ import React, {useState} from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from "leaflet";
 
+
 function MyMap() {
+
+  
+
   const [activePark, setActivePark] = useState(null);
   const icon = new Icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/784/784867.png",
     iconSize: [20, 20]
   });
 
+  
+
+
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+    <MapContainer 
+    center={[43.7101728, 7.2619532]} 
+    zoom={13} 
+    scrollWheelZoom={true}
+    whenReady={(map) => {
+      map.target.on("drag", function (e) {
+            console.log(map.target.getCenter())
+}
+          );
+        }}>
   <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   />
        <Marker
           position={[
-            45.323,
-            -75.25
+            43.7101728,
+            7.2619532
           ]}
           icon={icon}>
             <Popup>
