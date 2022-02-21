832e44e4f3513d1d5918ad96e4902000c8c1f23b
hamzaayoub98 <hamza.ayoub@etu.univ-cotedazur.fr>
2022-02-21 21:26:15 +0100

    Update table

--- frontend/src/components/table.jsx
+++ frontend/src/components/table.jsx
@@ -17,6 +17,15 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Container from '@material-ui/core/Container';
 import STATIONS from "../data/stations.mock";
 
+
+import { useTheme } from '@mui/material/styles';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
 const useRowStyles = makeStyles({
   root: {
     '& > *': {
@@ -116,6 +125,68 @@ function Row(props) {
   );
 }
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+
 /*Row.propTypes = {
   row: PropTypes.shape({
     calories: PropTypes.number.isRequired,
@@ -146,14 +217,35 @@ const rows = STATIONS;
 
 
 export default function CollapsibleTable() {
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
+    
     <div style={{ height: "100%", width: '100%',overflow:'scroll' }}>
+    <Typography variant="h2" gutterBottom component="div">
+                Stations
+              </Typography>
       <TableContainer component={Paper} >
         <Table aria-label="collapsible table">
           <TableHead>
             <TableRow>
               <TableCell />
-              <TableCell>Adresse</TableCell>
+              <TableCell align="left">Adresse</TableCell>
               <TableCell align="right">Ville</TableCell>
               <TableCell align="right">Code postal</TableCell>
               <TableCell align="right">Latitude</TableCell>
@@ -161,10 +253,33 @@ export default function CollapsibleTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
               <Row key={row.name} row={row} />
             ))}
           </TableBody>
+          <TableFooter >
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
         </Table>
       </TableContainer>
     </div>
--- frontend/src/data/stations.mock.jsx
+++ frontend/src/data/stations.mock.jsx
@@ -184,7 +184,193 @@ const STATIONS = [
         "_longitude": "7.29400",
         "_cp": "06300",
         "_pop": "R"
-     }
+     },
+     {
+      "adresse": "111, Avenue Saint-Augustin",
+      "ville": "Nice",
+      "services": {
+         "service": [
+            "Carburant additiv�",
+            "Toilettes publiques",
+            "Station de gonflage",
+            "Boutique non alimentaire",
+            "Vente de gaz domestique (Butane, Propane)",
+            "Piste poids lourds",
+            "Lavage manuel"
+         ]
+      },
+      "prix": [
+         {
+            "_nom": "Gazole",
+            "_id": "1",
+            "_maj": "2022-01-29T09:07:55",
+            "_valeur": "1.766"
+         },
+         {
+            "_nom": "SP95",
+            "_id": "2",
+            "_maj": "2022-01-19T12:31:38",
+            "_valeur": "1.835"
+         }
+      ],
+      "_id": "6296001",
+      "_latitude": "43.67750",
+      "_longitude": "7.22636",
+      "_cp": "06200",
+      "_pop": "R"
+   },
+   {
+      "adresse": "Place Max Barel",
+      "ville": "NICE",
+      "services": {
+         "service": [
+            "Carburant additiv�",
+            "Boutique alimentaire",
+            "Station de gonflage",
+            "Boutique non alimentaire",
+            "Services r�paration / entretien",
+            "Piste poids lourds",
+            "DAB (Distributeur automatique de billets)",
+            "Lavage automatique",
+            "Lavage manuel",
+            "Vente de fioul domestique"
+         ]
+      },
+      "prix": [
+         {
+            "_nom": "Gazole",
+            "_id": "1",
+            "_maj": "2022-02-09T06:48:50",
+            "_valeur": "1.779"
+         },
+         {
+            "_nom": "SP95",
+            "_id": "2",
+            "_maj": "2022-02-09T06:49:19",
+            "_valeur": "1.815"
+         },
+         {
+            "_nom": "SP98",
+            "_id": "6",
+            "_maj": "2022-02-09T06:49:54",
+            "_valeur": "1.908"
+         }
+      ],
+      "rupture": [
+         {
+            "_id": "5",
+            "_nom": "E10",
+            "_debut": "2014-06-26T15:27:00",
+            "_fin": ""
+         }
+      ],
+      "_id": "6300002",
+      "_latitude": "43.70049",
+      "_longitude": "7.28739",
+      "_cp": "06300",
+      "_pop": "R"
+   },
+   {
+      "adresse": "10 Corniche Andr� de Joly",
+      "ville": "Nice",
+      "horaires": {
+         "jour": [
+            {
+               "_id": "1",
+               "_nom": "Lundi",
+               "_ferme": ""
+            },
+            {
+               "_id": "2",
+               "_nom": "Mardi",
+               "_ferme": ""
+            },
+            {
+               "_id": "3",
+               "_nom": "Mercredi",
+               "_ferme": ""
+            },
+            {
+               "_id": "4",
+               "_nom": "Jeudi",
+               "_ferme": ""
+            },
+            {
+               "_id": "5",
+               "_nom": "Vendredi",
+               "_ferme": ""
+            },
+            {
+               "_id": "6",
+               "_nom": "Samedi",
+               "_ferme": ""
+            },
+            {
+               "_id": "7",
+               "_nom": "Dimanche",
+               "_ferme": ""
+            }
+         ],
+         "_automate-24-24": "1"
+      },
+      "services": {
+         "service": [
+            "Boutique alimentaire",
+            "Station de gonflage",
+            "Boutique non alimentaire",
+            "Services r�paration / entretien",
+            "Vente de gaz domestique (Butane, Propane)",
+            "Lavage automatique",
+            "Lavage manuel",
+            "Automate CB 24/24"
+         ]
+      },
+      "prix": [
+         {
+            "_nom": "Gazole",
+            "_id": "1",
+            "_maj": "2022-02-04T16:42:25",
+            "_valeur": "1.799"
+         },
+         {
+            "_nom": "SP95",
+            "_id": "2",
+            "_maj": "2022-02-01T08:43:51",
+            "_valeur": "1.889"
+         },
+         {
+            "_nom": "SP98",
+            "_id": "6",
+            "_maj": "2022-02-01T08:43:52",
+            "_valeur": "1.929"
+         }
+      ],
+      "rupture": [
+         {
+            "_id": "4",
+            "_nom": "GPLc",
+            "_debut": "2019-06-11T12:28:52",
+            "_fin": ""
+         },
+         {
+            "_id": "5",
+            "_nom": "E10",
+            "_debut": "2019-06-11T12:28:52",
+            "_fin": ""
+         },
+         {
+            "_id": "3",
+            "_nom": "E85",
+            "_debut": "2019-06-11T12:28:52",
+            "_fin": ""
+         }
+      ],
+      "_id": "6300012",
+      "_latitude": "43.70200",
+      "_longitude": "7.29400",
+      "_cp": "06300",
+      "_pop": "R"
+   }
 ]
 
 export default STATIONS;
